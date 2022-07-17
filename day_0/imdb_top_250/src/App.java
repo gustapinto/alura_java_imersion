b5784eac013a0a5d610c1724d0d5b53ec9c0382a
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-07-17 14:40:27 -0300

    Add IMDB top 250 movies api ingestion example

---
+++ day_0/imdb_top_250/src/App.java
@@ -0,0 +1,24 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        var url = "https://imdb-api.com/en/API/Top250Movies/k_slrga62w";
+        URI uri = URI.create(url); // Cria uma nova URI para uso no client
+
+        // Abre um cliente HTTP
+        var client = HttpClient.newHttpClient();
+
+        // Faz uma requisição GET criando request e response e as
+        // processando com o cliente HTTP
+        var request = HttpRequest.newBuilder(uri).build();
+        var responseHandler = HttpResponse.BodyHandlers.ofString();
+        var json = client.send(request, responseHandler).body();
+
+        // Exibe o json exraído da API do IMDB
+        System.out.println(json);
+    }
+}
+
